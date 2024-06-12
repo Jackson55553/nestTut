c1e29ff6901d2b4f9aacb7a96e3b62e23ecb5672
Evgeny Dudin <Jackson55553@gmail.com>
2024-06-12 20:41:38 +0500

    try config

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,7 @@ import { Token } from './tokens/tokens.model';
       database: process.env.POSTGRES_DB,
       models: [User, Token],
       autoLoadModels: true,
-      synchronize: true,
+      synchronize: false,
     }),
     WalletsModule,
     TokensModule,
